00d432cbf2e419306075a750c2ec665dc3a817e2
Jamie <jr-ingram@hotmail.co.uk>
2018-09-28 17:47:45 +0100

    Initial commit. Can run a basic random search

---
+++ src/lab/main/Main.java
@@ -0,0 +1,14 @@
+package lab.pkg1;
+
+import lab.pkg1.TravellingSalesman;
+import lab.pkg1.RouteSet;
+
+public class Main {
+	
+	private static int[][] graph;
+	private static RouteSet routes;
+	
+	public static void main(String[] args) {            
+            TravellingSalesman ts = new TravellingSalesman("/home/jamie/Documents/Programming/Lab 1/Lab 1/src/lab/pkg1/ulysses16.csv");
+	}
+}
