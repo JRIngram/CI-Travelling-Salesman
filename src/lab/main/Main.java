0b42d975a422f66e812f388623905092a47b5355
Jamie <jr-ingram@hotmail.co.uk>
2018-09-29 14:48:22 +0100

    Added relative pathing

--- src/lab/main/Main.java
+++ src/lab/main/Main.java
@@ -4,11 +4,11 @@ import lab.main.TravellingSalesman;
 import lab.main.RouteSet;
 
 public class Main {
-	
+
 	private static int[][] graph;
 	private static RouteSet routes;
-	
-	public static void main(String[] args) {            
-            TravellingSalesman ts = new TravellingSalesman("/home/jamie/Documents/University/Year 3/Computational Intelligence/Labs/CI-Travelling-Salesman/src/lab/main/ulysses16.csv");
+
+	public static void main(String[] args) {
+		TravellingSalesman ts = new TravellingSalesman("src/lab/main/ulysses16.csv");
 	}
 }
