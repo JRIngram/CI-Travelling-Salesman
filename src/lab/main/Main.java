d7b7681a2bc7a6d504e2b8caed4564c3ef5e9c21
Jamie <jr-ingram@hotmail.co.uk>
2018-10-04 20:41:14 +0100

    Random Search now runs in main

--- src/lab/main/Main.java
+++ src/lab/main/Main.java
@@ -9,10 +9,10 @@ public class Main {
 	private static RouteSet routes;
 
 	public static void main(String[] args) {
-		TravellingSalesman tsFixed = new TravellingSalesman();
-		System.out.println("Best route of Fixed: " + tsFixed.randomSearch(24));
-		TravellingSalesman tsUlysses = new TravellingSalesman("src/lab/main/ulysses16.csv");
-		System.out.println("Best route of Ulysses: " + tsUlysses.randomSearch(2500));
+		RandomSearch randFixed = new RandomSearch();
+		System.out.println("Best route of Fixed: " + randFixed.randomSearch(24).getItemTwo());
+		RandomSearch randUlysses = new RandomSearch("src/lab/main/ulysses16.csv");
+		System.out.println("Best route of Ulysses: " + randUlysses.randomSearch(2500).getItemTwo());
 		
 	}
 }
