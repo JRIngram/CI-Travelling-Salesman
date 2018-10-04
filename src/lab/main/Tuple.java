08f15eabc079beb314c02ec54f46de09bf4ac943
Jamie <jr-ingram@hotmail.co.uk>
2018-10-04 19:55:52 +0100

    Create Tuple class

---
+++ src/lab/main/Tuple.java
@@ -0,0 +1,47 @@
+package lab.main;
+
+public class Tuple<T,E> {
+
+	private T itemOne;
+	private E itemTwo;
+	
+	public Tuple(T itemOne, E itemTwo){
+		if(itemOne != null) {
+			this.itemOne = itemOne;
+		}
+		if(itemTwo != null) {
+			this.itemTwo = itemTwo;
+		}
+	}
+	
+	public boolean setItemOne(T itemOne){
+		try {
+			this.itemOne = itemOne;	
+			return true;
+		}
+		catch(Exception e){
+			System.out.println("An error has occurred when setting Item One!\n" + e.toString());
+			return false;
+		}
+	}
+
+	public T getItemOne() {
+		return itemOne;
+	}
+
+	public boolean setItemTwo(E itemTwo) {
+		try {
+			this.itemTwo = itemTwo;	
+			return true;
+		}
+		catch(Exception e){
+			System.out.println("An error has occurred when setting Item Two!\n" + e.toString());
+			return false;
+		}
+	}
+	
+	public E getItemTwo() {
+		return itemTwo;
+	}
+
+}
