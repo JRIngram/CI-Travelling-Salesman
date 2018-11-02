ab11f12a2271605ae75bc4d5eaa0805ff46c2dca
JRIngram <jr-ingram@hotmail.co.uk>
2018-11-02 10:12:41 +0000

    Updated Docs for Tuple

--- src/lab/main/Tuple.java
+++ src/lab/main/Tuple.java
@@ -1,10 +1,22 @@
 package lab.main;
 
+/**
+ * A class which represents a Tuple datatype.
+ * @author JRIngram
+ * @param <T> A generic data type.
+ * @param <E> A generic data type.
+ */
 public class Tuple<T,E> implements Cloneable {
 
 	private T itemOne;
 	private E itemTwo;
 	
+	/**
+	 * Creates a tuple with two points of data.
+	 * If one of the data points is left blank, then the value of that data point will stay as null until set. 
+	 * @param itemOne First item of data to load in.
+	 * @param itemTwo Second item of data to load in.
+	 */
 	public Tuple(T itemOne, E itemTwo){
 		if(itemOne != null) {
 			this.itemOne = itemOne;
@@ -14,6 +26,11 @@ public class Tuple<T,E> implements Cloneable {
 		}
 	}
 	
+	/**
+	 * Sets the value of itemOne in the tuple
+	 * @param itemOne The new data to be loaded into itemOne
+	 * @return true if the change has been successful; false if the change has been unsuccessful.
+	 */
 	public boolean setItemOne(T itemOne){
 		try {
 			this.itemOne = itemOne;	
@@ -24,11 +41,20 @@ public class Tuple<T,E> implements Cloneable {
 			return false;
 		}
 	}
-
+	
+	/**
+	 * Returns the value of itemOne in the tuple.
+	 * @return the value of itemOne in the tuple.
+	 */
 	public T getItemOne() {
 		return itemOne;
 	}
 
+	/**
+	 * Sets the value of itemTwo in the tuple
+	 * @param itemOne The new data to be loaded into itemTwo
+	 * @return true if the change has been successful; false if the change has been unsuccessful.
+	 */
 	public boolean setItemTwo(E itemTwo) {
 		try {
 			this.itemTwo = itemTwo;	
@@ -40,10 +66,17 @@ public class Tuple<T,E> implements Cloneable {
 		}
 	}
 	
+	/**
+	 * Returns the value of itemTwo in the tuple.
+	 * @return the value of itemTwo in the tuple.
+	 */
 	public E getItemTwo() {
 		return itemTwo;
 	}
 	
+	/**
+	 * Clones the Tuple.
+	 */
 	public Object clone() throws CloneNotSupportedException {
 		return super.clone();
 	}
